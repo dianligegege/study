6b1b42ec3257d8e223546683a60c508d3eef76b7
dianligegege <1220671124@qq.com>
2019-04-24 15:53:16 +0800

    提交

--- webpack-demo/webpack.common.js
+++ webpack-demo/webpack.common.js
@@ -1,20 +1,19 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: {
-        app: './src/index.js',
-        another: './src/another-module.js'
+        app: './src/index.js'
     },
     plugins: [
-        new CleanWebpackPlugin(),
-        new HtmlWebpackPlugin({
+            new CleanWebpackPlugin(['dist']),
+            new HtmlWebpackPlugin({
             title: 'Production'
-        }),
+        })
     ],
     output: {
         filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-}
+        path: path.resolve(__dirname, 'dist')
+    }
+};
--- webpack-demo/webpack.prod.js
+++ webpack-demo/webpack.prod.js
@@ -1,19 +1,6 @@
 const merge = require('webpack-merge');
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const common = require('./webpack.common.js');
-const webpack = require('webpack');
 
 module.exports = merge(common, {
-    devtool: 'source-map',
-    plugins: [
-        new UglifyJSPlugin({
-            sourceMap: true
-        }),
-        new webpack.DefinePlugin({
-            'process.env.NODE_ENV': JSON.stringify('production')
-        }),
-        new webpack.optimize.CommonsChunkPlugin({
-            name: 'common'
-        })
-    ]
+    mode: 'production',
 });
